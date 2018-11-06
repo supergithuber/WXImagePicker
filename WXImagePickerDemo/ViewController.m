3e9e51df153ee314acb08ed1f128499acbdd8039
sivanWu <1984430988@qq.com>
2018-11-06 16:14:16 +0800

    structure

---
+++ WXImagePickerDemo/ViewController.m
@@ -0,0 +1,38 @@
+//
+//  ViewController.m
+//  WXImagePicker
+//
+//  Created by HFY on 2018/11/6.
+//  Copyright © 2018年 wuxi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self setupView];
+}
+
+- (void)setupView {
+    UIButton * button = [UIButton buttonWithType:UIButtonTypeCustom];
+    button.frame = CGRectMake(0, 0, 200, 100);
+    button.center = CGPointMake(CGRectGetWidth(self.view.bounds) / 2.0, CGRectGetHeight(self.view.bounds) / 2.0);
+    [button setTitle:@"open Album" forState:UIControlStateNormal];
+    [button addTarget:self action:@selector(openAlbum:) forControlEvents:UIControlEventTouchUpInside];
+    button.backgroundColor = [UIColor redColor];
+    button.layer.cornerRadius = 4;
+    button.layer.masksToBounds = YES;
+    [self.view addSubview:button];
+}
+
+- (void)openAlbum:(UIButton *)sender{
+    
+}
+
+@end
