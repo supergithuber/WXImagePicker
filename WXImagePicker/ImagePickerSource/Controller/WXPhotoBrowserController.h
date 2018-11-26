053c9a6db859bfdbf343db1c897df52b68fa1d45
sivanWu <1984430988@qq.com>
2018-11-26 17:44:16 +0800

    add file browser

---
+++ WXImagePicker/ImagePickerSource/Controller/WXPhotoBrowserController.h
@@ -0,0 +1,17 @@
+//
+//  WXPhotoBrowserController.h
+//  WXImagePicker
+//
+//  Created by HFY on 2018/11/26.
+//  Copyright © 2018年 wuxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WXPhotoBrowserController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ WXImagePicker/ImagePickerSource/Controller/WXPhotoBrowserController.m
@@ -0,0 +1,32 @@
+//
+//  WXPhotoBrowserController.m
+//  WXImagePicker
+//
+//  Created by HFY on 2018/11/26.
+//  Copyright © 2018年 wuxi. All rights reserved.
+//
+
+#import "WXPhotoBrowserController.h"
+
+@interface WXPhotoBrowserController ()
+
+@end
+
+@implementation WXPhotoBrowserController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
